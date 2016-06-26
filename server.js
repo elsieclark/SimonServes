63c41716a6e906315bc18ec9ebca19ad1f76c9d6
willdavidc <willdavidc@gmail.com>
2016-06-26 02:49:08 -0400

    Change testing parameters
    Change default parameters for testing.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 var phoneSequence = ""
-var piSequence = "hi"
+var piSequence = ""
 
 var arduinoTurn = false
 
