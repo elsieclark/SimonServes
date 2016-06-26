2ab7afe1d3df15910379d50a3daaaa144d403251
willdavidc <willdavidc@gmail.com>
2016-06-26 06:34:51 -0400

    update
    update

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var val = "-1"
+'use strict';
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+var phoneSequence = ""
+var piSequence = ""
+
+var arduinoTurn = false
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
 
 io.on('connection', function(socket){
     
@@ -15,24 +26,27 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
     
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    
+    
+  socket.on('sequencePhoneToServer', function(msg){
+      if (msg != phoneSequence && !arduinoTurn) {
+          console.log('Phone sequence: ' + msg);
+          phoneSequence = msg;
+      }
   });
     
+});
     
-  socket.on('incrementor', function(msg){
-      if (val != msg) {
-          console.log('Incrementor: ' + msg);
-          val = msg;
+  socket.on('sequencePhoneToServer', function(msg){
+      if (msg != phoneSequence && !arduinoTurn) {
+          console.log('Phone sequence: ' + msg);
+          phoneSequence = msg;
       }
   });
     
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-// http://[2620:101:f000:700:c9b8:4484:a65d:df7f]:3000/
-
-setInterval(() => io.emit('sequencepi', new Date().toTimeString()), 50);
+setInterval(function(){
+    io.emit('sequenceServerToPhone', piSequence)
+    io.emit('sequenceServerToPi', phoneSequence)
+}, 1000);
